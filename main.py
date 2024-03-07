560cfadac1e5323c17bd9094ca7a173c11bc77c7
AlexisRavel <alexis.ravel@outlook.fr>
2024-03-07 16:06:21 +0100

    Update du neurone

--- main.py
+++ main.py
@@ -1,30 +1,35 @@
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as graph
 
 from neurone import Neurone
 from sklearn.datasets import make_blobs
 
 
-## Main programme
-# Généré les données de base, le dataSet
-X, Y = make_blobs(n_samples=100, n_features=2, centers=2, random_state=0)
-Y = Y.reshape((Y.shape[0], 1))
+def main():
+    ## Datasets
+    X, Y = make_blobs(n_samples=100, n_features=2, centers=2, random_state=0)
+    Y = Y.reshape((Y.shape[0], 1))
 
-# Neurone
-n = Neurone(X, Y)
-premierPred = n._learn(100)
-print(premierPred)
+    ## Programme
+    n = Neurone(X, Y)
+    pred = n._learn(1000)
 
-plt.plot(n.cout)
-plt.show()
+    ## Graphe
+    # Fonction coût
+    graph.plot(n.cout)
+    graph.title(str(pred*100) + "%")
+    graph.show()
 
-newData = np.array([2, 1])
-print(n._predict(newData))
+    graph.plot(n.predictions)
+    graph.show()
 
-x0 = np.linspace(-2, 5, 100)
-x1 = (-n.W[0] * x0 - n.b) / n.W[1]
+    # Frontière de décision
+    x0 = np.linspace(-2, 5, 100)
+    x1 = (-n.W[0] * x0 - n.b) / n.W[1]
 
-plt.scatter(X[:, 0], X[:, 1], c=Y, cmap="summer")
-plt.scatter(newData[0], newData[1], c='r')
-plt.plot(x0, x1, c='orange', lw=3)
-plt.show()
+    graph.scatter(X[:, 0], X[:, 1], c=Y, cmap="summer")
+    graph.plot(x0, x1, c='blue', lw=2)
+    graph.show()
+
+
+main()
--- neurone.py
+++ neurone.py
@@ -1,83 +1,101 @@
 import numpy as np
 
+from tqdm import tqdm
 from sklearn.metrics import accuracy_score
 
 
 class Neurone:
-    ## Variables ##
+    ## Variables
     # Données
-    # X:ndarray la matrice des données
-    # Y:ndarray la matrice des "étiquettes" des données correspondantes (0 ou 1)
-    
+    X: np.ndarray
+    Y: np.ndarray
+    Xt = 0
+    Yt = 0
+
     # Paramètres
-    W = None # la matrice des paramètres de poids
-    b = 0 # le paramètre du biais
-    learningRate = 0 # le pas positif pour mettre l'avancer du programme d'apprentissage du neurone
+    W: np.ndarray
+    b: np.ndarray
 
-    # Résultat des fonctions
-    A = None # la matrice du modèle0
-    cout = [] # le cout stocker à chaque itération
+    # Travail neurone
+    learningRate: float
+    A: np.ndarray
+    cout = [] # Stocker l'évolution du cout du modele
+    coutTest = [] # Stocker l'évolution du cout du modele avec les données de test
+    predictions = [] # Stocker l'évolution de la vraisemblance du modèle
+    predictionsTest = [] # Stocker l'évolution de la vraisemblance du modèle avec les données de test
 
-    ## Fonctions ##
-    # Entrée: la matrice X des données et sa matrice Y associé
-    # Traitement: Initialise les paramètres en fonction des données
-    # Sortie: Rien   
-    def __init__(self, matX, matY, lR=0.2):
-        # init des données
+    ## Fonctions
+    # Entrée: matX, matY --> nos données d'entrainements
+    #         testX, testY --> des données de test pour diagnostiquer le surapprentissage
+    #         learningRate --> Pas d'apprentissage
+    # Traitement: Init les variables de données X et Y ainsi que les paramètres W et b
+    # Sortie: Rien
+    def __init__(self, matX:np.ndarray, matY:np.ndarray, testX:np.ndarray=None, testY:np.ndarray=None, lR=0.2):
         self.X = matX
         self.Y = matY
+        if type(testX)==np.ndarray and type(testY)==np.ndarray:
+            self.Xt = testX
+            self.Yt = testY
         self.learningRate = lR
 
-        # peuplement des paramètres
         self.W = np.random.randn(self.X.shape[1], 1)
         self.b = np.random.randn(1)
-
-    # Entrée: la matrice X de données sur laquelle définir le modèle
-    # Traitement: Calculer la "position" de la donnée par rapport à la frontière de décision Z et la probabilité A de cette dernière
-    # Sortie: A   
-    def _linear_model(self, X):
-        # Z = X*W + b
+    
+    # Modele linéaire #
+    # Entrée: X les données à traiter sur le modèle
+    # Traitement: Calcul de Z et de A, le modèle
+    # Sortie: A
+    def _calcul_modele(self, X):
+        # Z = X * W + b
         Z = X.dot(self.W) + self.b
-        # A = 1/1+e^(-Z)
+        # A = 1 / 1 + e^-Z
         return 1/(1+np.exp(-Z))
 
     # Entrée: Rien
-    # Traitement: Calcul le cout (le taux d'erreur) du modèle
-    # Sortie: Rien   
-    def _update_cost(self):
-        A = self._linear_model(self.X)
-        self.cout.append((-1/self.Y.size)*(np.sum(self.Y*np.log(A)+(1-self.Y)*np.log(1-A))))
-
-    # Entrée: Rien
-    # Traitement: Calculer les dérivées partielles afin de dterminer l'évolution de la courbe et donc des paramètres
-    # Sortie: dW et db les valeurs des dérivées partielles des paramètres W et b
-    def _gradient(self):
-        A = self._linear_model(self.X)
-        dW = (1/self.Y.size)*np.dot(self.X.T, A-self.Y)
-        db = (1/self.Y.size)*np.sum(A-self.Y)
+    # Traitement: Calcul les gradients afin de pouvoir ajuster les param
+    # Sortie: Le tuple de gradient (dW, db)
+    def _gradients(self):
+        dW = (1/self.Y.size)*np.dot(self.X.T, self.A-self.Y)
+        db = (1/self.Y.size)*np.sum(self.A-self.Y)
         return (dW, db)
-    
-    # Entrée: Rien
-    # Traitement: Mettre à jour les paramètres W et b en fonction des gradients
-    # Sortie: Rien  
-    def _update(self):
-        self._update_cost()
-        gradients = self._gradient()
+
+    # Entrée: Rien 
+    # Traitement: Ajuste les param en fonction des gradients
+    # Sortie: Rien
+    def _update_param(self):
+        gradients = self._gradients()
         self.W = self.W - self.learningRate * gradients[0]
         self.b = self.b - self.learningRate * gradients[1]
 
-    # Entrée: Nombre d'itération
-    # Traitement: Selon le nombre d'itération voulue i, fait tourner update() xi, plsu calcul la prediction du modèle
-    # Sortie: predOfY la vraisemblance de notre modèle  
-    def _learn(self, iteration):
-        for i in range(iteration):
-            self._update()
-        predOfY = self._predict(self.X)
-        return(accuracy_score(self.Y, predOfY))
+    # Log Loss #
+    # Entrée: A --> la probabilité sur laquelle on calcule le coût
+    # Traitement: Calcul le coût du modèle A calculer préalablement
+    # Sortie: L --> Le coût calculer
+    def _cout(self, A, Y):
+        # Epsilon --> éviter d'avoir un 0 dans le log
+        epsilon = 1e-15
+        return -(1/Y.size)*(np.sum(Y*np.log(A+epsilon)+(1-Y)*np.log(1-A+epsilon)))
 
-    # Entrée: La donnée X à traiter, si X=none, on prends alors la matrice X de données du neurone qui a été init
-    # Traitement: Selon le nombre d'itération voulue i, fait tourner update() xi
-    # Sortie: Rien  
+    # Entrée: Le nombre d'itération
+    # Traitement: Update les paramètres et calcul le cout du modele
+    # Sortie: Rien
+    def _learn(self, iter):
+        for i in tqdm(range(iter)):
+            self.A = self._calcul_modele(self.X)
+            self._update_param()
+            self.cout.append(self._cout(self.A, self.Y))
+            self.predictions.append(accuracy_score(self.Y, self._predict(self.X)))
+            # On calcul aussi le cout et la vraisemblance avec les données test si on en a
+            if type(self.Xt)==np.ndarray:
+                self.coutTest.append(self._cout(self._calcul_modele(self.Xt), self.Yt))
+                self.predictionsTest.append(accuracy_score(self.Yt, self._predict(self.Xt)))
+        # Le résultat de l'apprentissage
+        predOfY = self._predict(self.X)
+        return accuracy_score(self.Y, predOfY)
+    
+    # Entrée: Les données X à prédire
+    # Traitement: Prédiciton des données
+    # Sortie: Prédiction des données
     def _predict(self, X):
-        A = self._linear_model(X)
+        A = self._calcul_modele(X)
         return A >= 0.5
